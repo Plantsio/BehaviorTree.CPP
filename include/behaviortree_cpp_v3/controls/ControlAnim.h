fb07b5ad2c1213023ebf6a025653b020ec491af8
Gaoran Sun <33003517+shb3014@users.noreply.github.com>
2022-03-19 21:25:19 +0800

    add ControlAnim

---
+++ include/behaviortree_cpp_v3/controls/ControlAnim.h
@@ -0,0 +1,49 @@
+//
+// Created by Gordon on 2022/3/18.
+//
+
+#ifndef PLANTSIO_IVY_CONTROLANIM_H
+#define PLANTSIO_IVY_CONTROLANIM_H
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+#define ANIMATION_CONTROL_PRIORITY "priority"
+#define ANIMATION_NODE_N "n"
+
+#include "Anim/Anim.h"
+
+namespace BT {
+    class ControlAnim : public ControlNode {
+    public:
+        ControlAnim(const std::string &name, const NodeConfiguration &config) : ControlNode(name, config) {
+            setRegistrationID("ControlAnim");
+        }
+
+        static PortsList providedPorts() {
+            return {InputPort<int>(ANIMATION_CONTROL_PRIORITY)};
+        }
+
+        void halt() override;
+
+    private:
+
+        void set_status(Anim::anim_complete_ret t_status) {
+            if (t_status == Anim::success) {
+                setStatus(NodeStatus::SUCCESS);
+            } else if (t_status == Anim::failure || t_status == Anim::interrupted) {
+                setStatus(NodeStatus::FAILURE);
+            }
+        }
+
+        NodeStatus onStart();
+
+//        NodeStatus onRunning();
+
+        void onHalted();
+
+        NodeStatus tick() override;
+    };
+}
+
+#endif //PLANTSIO_IVY_CONTROLANIM_H
---
+++ src/controls/ControlAnim.cpp
@@ -0,0 +1,65 @@
+//
+// Created by Gordon on 2022/3/18.
+//
+
+#include "behaviortree_cpp_v3/controls/ControlAnim.h"
+#include "Anim/drivers/EmoDriver.h"
+
+BT::NodeStatus BT::ControlAnim::onStart() {
+    auto priority = getInput<int>(ANIMATION_CONTROL_PRIORITY);
+    Anim anim = Anim({},priority.value(),[this](Anim::anim_complete_ret ret){this->set_status(ret);});
+    for (auto child:children()){
+        auto n = child->getInput<int>(ANIMATION_NODE_N);
+        anim.add_unit(AnimUnit(child->name(),n.value()));
+        log_d("debug-bt add %s | %d",child->name().c_str(),n.value());
+    }
+    bool ret = EmoDriver::instance().set_current_anim(anim);
+    if (ret) {
+        return NodeStatus::RUNNING;
+    }else{
+        return NodeStatus::FAILURE;
+    }
+}
+
+//BT::NodeStatus BT::AnimationNode::onRunning() {
+//    return status();
+//}
+
+void BT::ControlAnim::onHalted() {
+    log_d("debug-bt halt");
+
+}
+
+BT::NodeStatus BT::ControlAnim::tick() {
+    const NodeStatus initial_status = status();
+
+    if( initial_status == NodeStatus::IDLE )
+    {
+        NodeStatus new_status = onStart();
+        return new_status;
+    }
+    //------------------------------------------
+//    if( initial_status == NodeStatus::RUNNING )
+//    {
+//        NodeStatus new_status = onRunning();
+//        if( new_status == NodeStatus::IDLE)
+//        {
+//            throw std::logic_error("AsyncActionNode2::onRunning() must not return IDLE");
+//        }
+//        return new_status;
+//    }
+    //------------------------------------------
+    return initial_status;
+
+
+    return BT::NodeStatus::SUCCESS;
+}
+
+
+void BT::ControlAnim::halt() {
+    if( status() == NodeStatus::RUNNING)
+    {
+        onHalted();
+    }
+    setStatus(NodeStatus::IDLE);
+}
